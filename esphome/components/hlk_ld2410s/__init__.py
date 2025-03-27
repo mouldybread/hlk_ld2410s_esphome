b7d7e25777c1be7e01804432ff88db51c58ac418
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-27 09:58:02 +0800

    Update __init__.py

--- esphome/components/hlk_ld2410s/__init__.py
+++ esphome/components/hlk_ld2410s/__init__.py
@@ -1,15 +1,14 @@
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome import core as cg
-from esphome import automation
+from esphome import core, automation
 
-HLK_LD2410SComponent = cg.global_ns.class_("HLK_LD2410S", cg.Component, cg.UARTDevice)
+from .hlk_ld2410s import HLK_LD2410S
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(HLK_LD2410SComponent),
+    cv.GenerateID(): cv.declare_id(HLK_LD2410S),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_parent_id(config[CONF_ID]))
-    cg.add_component(var, config)
+    var = core.Component.new(config[CONF_ID])
+    core.add(var.set_parent_id(config[CONF_ID]))
+    core.add_component(var, config)
