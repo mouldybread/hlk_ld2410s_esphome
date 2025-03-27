f0e27fa2f00ecd09ca3dbd61a5a546a03ef20662
mouldybread <38598858+mouldybread@users.noreply.github.com>
2025-03-27 09:54:02 +0800

    init

--- esphome/components/hlk_ld2410s/__init__.py
+++ esphome/components/hlk_ld2410s/__init__.py
@@ -1,13 +1,17 @@
-from .hlk_ld2410s import HLK_LD2410S
-import esphome.config_validation as cv
+from esphome import automation
 from esphome.const import CONF_ID
-from esphome import cg, automation
+import esphome.config_validation as cv
+from esphome.core import CORE
+from esphome.components import sensor
+from esphome.components.hlk_ld2410s import hlk_ld2410s
+
+HLK_LD2410SComponent = hlk_ld2410s.HLK_LD2410S
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(HLK_LD2410S),
+    cv.GenerateID(): cv.declare_id(HLK_LD2410SComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    hlk_ld2410s = cg.new_Pvariable(config[CONF_ID])
-    cg.add(hlk_ld2410s.set_parent_id(config[CONF_ID]))
-    cg.add_component(hlk_ld2410s, config)
+    var = cg.new_Pvariable(config[CONF_ID])
+    cg.add(var.set_parent_id(config[CONF_ID]))
+    cg.add_component(var, config)
